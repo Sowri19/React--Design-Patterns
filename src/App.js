4307443b30ab56e923582eccec5c67b5fe3a1d45
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-07-27 23:59:32 -0400

    UncontrolledOnboarding

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import { Uncontrolled } from "./Uncontrolled";
-import { Controlled } from "./ControlledForm";
-import { UncontrolledModal } from "./UncontrolledModal";
-import { useState } from "react";
-import { ControlledModal } from "./ControlledModal";
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
 
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step One</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step Two</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
+const StepThree = ({ goToNext }) => (
+  <>
+    <h1>Step Three</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+);
 function App() {
-  const [shouldShow, setShouldShow] = useState(false);
-
   return (
-    <>
-      <ControlledModal
-        shouldShow={shouldShow}
-        onRequestClose={() => setShouldShow(false)}
-      >
-        <h1>Hello!</h1>
-      </ControlledModal>
-      <button onClick={() => setShouldShow(!shouldShow)}>
-        {shouldShow ? "Hide Modal" : "Show Modal"}
-      </button>
-    </>
+    <UncontrolledOnboardingFlow>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnboardingFlow>
   );
 }
 
 export default App;
-
-// we are controlling the modals from the parent component.
