dcb2143dcb14a59ebe4533bb0ce646629188940e
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-07-27 21:44:44 -0400

    controlled-uncontrolledmodals

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import { Uncontrolled } from "./Uncontrolled";
 import { Controlled } from "./ControlledForm";
+import { UncontrolledModal } from "./UncontrolledModal";
+import { useState } from "react";
+import { ControlledModal } from "./ControlledModal";
 
 function App() {
-  return <Controlled />;
+  const [shouldShow, setShouldShow] = useState(false);
+
+  return (
+    <>
+      <ControlledModal
+        shouldShow={shouldShow}
+        onRequestClose={() => setShouldShow(false)}
+      >
+        <h1>Hello!</h1>
+      </ControlledModal>
+      <button onClick={() => setShouldShow(!shouldShow)}>
+        {shouldShow ? "Hide Modal" : "Show Modal"}
+      </button>
+    </>
+  );
 }
 
 export default App;
+
+// we are controlling the modals from the parent component.
