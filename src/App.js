4a80bfcee8ef348abc9b3f56d33fe5471aa1cf52
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-07-28 23:41:25 -0400

    uncontrolledOnboardingflow

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
 const StepOne = ({ goToNext }) => (
   <>
     <h1>Step One</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ name: "sowri" })}>Next</button>
   </>
 );
 const StepTwo = ({ goToNext }) => (
   <>
     <h1>Step Two</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ age: 100 })}>Next</button>
   </>
 );
 const StepThree = ({ goToNext }) => (
   <>
     <h1>Step Three</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ hairColor: "brown" })}>Next</button>
   </>
 );
 function App() {
   return (
-    <UncontrolledOnboardingFlow>
+    <UncontrolledOnboardingFlow
+      onFinish={(data) => {
+        console.log(data);
+        alert("onboarding complete!");
+      }}
+    >
       <StepOne />
       <StepTwo />
       <StepThree />
--- src/UncontrolledOnboardingFlow.js
+++ src/UncontrolledOnboardingFlow.js
@@ -4,9 +4,28 @@ export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
   const [onboardingData, setOnboardingData] = useState({});
   const [currentIndex, setCurrentIndex] = useState(0);
   const currentChild = React.Children.toArray(children)[currentIndex];
+  // const [stepData, setStepData] = useState({});
 
-  const goToNext = () => {
-    setCurrentIndex(currentIndex + 1);
+  const goToNext = (stepData) => {
+    const nextIndex = currentIndex + 1;
+
+    // I can also add and input here to get the stepData from the user.
+    const updatedData = {
+      ...onboardingData,
+      ...stepData,
+    };
+    // ... is spread operator, which combines two objects into one.(updated data is the combination of onboardingData and stepData)
+
+    console.log(updatedData);
+
+    if (nextIndex < children.length) {
+      setCurrentIndex(nextIndex);
+    } else {
+      onFinish(updatedData);
+    }
+
+    // we are checking weather the next index is in the bounds of the children array, if it is then we are setting the next index as the current index, if not then we are calling the onFinish function and passing the updated data as the argument.
+    setOnboardingData(updatedData);
   };
 
   if (React.isValidElement(currentChild)) {
@@ -15,3 +34,5 @@ export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
 
   return currentChild;
 };
+
+//  the main app.js has very little control over the data that is being passed to the onboarding flow. The onboarding flow is responsible for collecting the data and passing it back to the main app.js. This is the main difference between controlled and uncontrolled components. In controlled components, the main app.js is responsible for collecting the data, and in uncontrolled components, the component itself is responsible for collecting the data.
