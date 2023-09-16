92b12941f3c22264e6ced949977ba3fcbaa7fc2e
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-09-16 14:00:57 -0400

    sending props through HOC

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import { SplitScreen } from "./SplitScreen";
+import { printProps } from "./printProps";
+import { UserInfo } from "./UserInfo";
 
-const LeftHandComponent = ({ name }) => {
-  return <h1 style={{ backgroundColor: "green" }}>{name}</h1>;
-};
-const RightHandComponent = ({ message }) => {
-  return <h1 style={{ backgroundColor: "yellow" }}>{message}</h1>;
-};
-// the leftHandComponent and RightHandComponent are the children of the SplitScreen component instead
-// of props to the components
+const UserInfoWrapped = printProps(UserInfo);
+
 function App() {
-  return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent name="sowri" />
-      <RightHandComponent message="this is awesome" />
-    </SplitScreen>
-  );
+  return <UserInfoWrapped a={1} b="Hello" c={{ name: "sowri" }} />;
 }
 
 export default App;
